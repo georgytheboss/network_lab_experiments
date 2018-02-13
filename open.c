d7c057438c1b0e9212019ff867614bcce75477c8
Georgy <georgyjose007@gmail.com>
2018-02-13 22:42:06 +0530

    Can read first element in the array

--- open.c
+++ open.c
@@ -1,10 +1,12 @@
-// ;gcc create.c -o create
+// gcc create.c -o create
 #include <stdio.h>
 #include <fcntl.h>
+#include <stdlib.h>
 
 void main(){
-	char *c = (char *) calloc(100,sizeof(char));
+	char *c[]= (char *) calloc(100,sizeof(char[10]));;
 	int fd = open("abc.txt",O_RDWR|O_CREAT);
-	// printf("%c\n",fd[0] );
+	int sz = read(fd, &c, 100);
+	printf("%s\n",c[0]);
 	// printf("fd = %d\n",fd);
 }
