6d7ac763995adbd6a6664618649d1a8e37bcc514
Mark Zukerberg <zukerberg@mark.com>
2018-02-28 12:27:03 +0530

    Comment added to identify writer code and reader code

--- queue_reader_writer.c
+++ queue_reader_writer.c
@@ -18,9 +18,12 @@ void main(){
 	gets(message.mesg_text);
 	msgsnd(msgid, &message, sizeof(message), 0);
 	printf("Data send is: %s \n", message.mesg_text);
+	//Data Transmission completed
+
 	key = ftok("progfile", 65);
 	msgid = msgget(key, 0666 | IPC_CREAT);
 	msgrcv(msgid, &message, sizeof(message), 1, 0);
 	printf("Data Received is: %s \n", message.mesg_text);
 	msgctl(msgid, IPC_RMID, NULL);
+	//Data Received
 	}
