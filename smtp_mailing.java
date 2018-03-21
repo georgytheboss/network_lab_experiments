32df131855ab41a21ceaebc4933f6246f1f94309
Steve Jobs <jobs@apple.com>
2018-03-21 12:04:35 +0530

    Working on smtp

--- smtp_mailing.java
+++ smtp_mailing.java
@@ -1,32 +1,40 @@
-import java.util.*;  
-import javax.mail.*;  
-import javax.mail.internet.*;  
-import javax.activation.*;  
-  
-public class SendEmail  
-{  
- public static void main(String [] args){  
-      String to = "sonoojaiswal1988@gmail.com";//change accordingly  
-      String from = "sonoojaiswal1987@gmail.com";change accordingly  
-      String host = "localhost";//or IP address  
-  
-     //Get the session object  
-      Properties properties = System.getProperties();  
-      properties.setProperty("mail.smtp.host", host);  
-      Session session = Session.getDefaultInstance(properties);  
-  
-     //compose the message  
-      try{  
-         MimeMessage message = new MimeMessage(session);  
-         message.setFrom(new InternetAddress(from));  
-         message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));  
-         message.setSubject("Ping");  
-         message.setText("Hello, this is example of sending email  ");  
-  
-         // Send message  
-         Transport.send(message);  
-         System.out.println("message sent successfully....");  
-  
-      }catch (MessagingException mex) {mex.printStackTrace();}  
-   }  
+import java.util.Properties;    
+import javax.mail.*;    
+import javax.mail.internet.*;    
+class smtp_mailing{  
+    public static void send(String from,String password,String to,String sub,String msg){  
+          //Get properties object    
+          Properties props = new Properties();    
+          props.put("mail.smtp.host", "smtp.gmail.com");    
+          props.put("mail.smtp.socketFactory.port", "465");    
+          props.put("mail.smtp.socketFactory.class",    
+                    "javax.net.ssl.SSLSocketFactory");    
+          props.put("mail.smtp.auth", "true");    
+          props.put("mail.smtp.port", "465");    
+          //get Session   
+          Session session = Session.getDefaultInstance(props,    
+           new javax.mail.Authenticator() {    
+           protected PasswordAuthentication getPasswordAuthentication() {    
+           return new PasswordAuthentication(from,password);  
+           }    
+          });    
+          //compose message    
+          try {    
+           MimeMessage message = new MimeMessage(session);    
+           message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));    
+           message.setSubject(sub);    
+           message.setText(msg);    
+           //send message  
+           Transport.send(message);    
+           System.out.println("message sent successfully");    
+          } catch (MessagingException e) {throw new RuntimeException(e);}    
+             
+    }  
 }  
+public class SendMailSSL{    
+ public static void main(String[] args) {    
+     //from,password,to,subject,message  
+     Mailer.send("from@gmail.com","xxxxx","to@gmail.com","hello javatpoint","How r u?");  
+     //change from, password and to  
+ }    
+}    
