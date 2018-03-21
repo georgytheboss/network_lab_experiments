7441a4b0d8234bbca321fb3f8956a52dc0e4d182
Mark Zukerberg <zukerberg@mark.com>
2018-03-21 10:27:31 +0530

    UDP palindrome program completed

---
+++ udpclient.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.net.*;
+
+public class udpclient{
+	public static void main(String args[]){
+		try{ 
+			InetAddress ip = InetAddress.getByName("127.0.0.1");
+			DatagramSocket ds = new DatagramSocket();
+			System.out.print("Enter message: ");
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			String msg = br.readLine();
+			DatagramPacket dp = new DatagramPacket(msg.getBytes(),msg.length(),ip,3000);
+			ds.send(dp);
+			DatagramSocket ds2 = new DatagramSocket(4000);
+			byte buf[]=new byte[1024];
+			DatagramPacket dp2 = new DatagramPacket(buf,1024);
+			ds2.receive(dp2);
+			String msg2 = new String(dp2.getData(),0,dp2.getLength());
+			System.out.println("Message returned from Server ::"+msg+" "+msg2);
+			}
+
+		catch(Exception e){
+			System.out.println("Error");
+			}
+		}
+	}	
---
+++ udpserver.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class udpserver{
+	public static void main(String args[]){
+		try{
+			DatagramSocket ds = new DatagramSocket(3000);
+			byte buf[]=new byte[1024];
+			DatagramPacket dp = new DatagramPacket(buf,1024);
+			ds.receive(dp);
+			String msg = new String(dp.getData(),0,dp.getLength());
+			System.out.print("Received message "+msg);
+			String  msg2,rev = "";
+			int length = msg.length();
+			for(int i=length-1;i>=0;i--)
+				rev = rev + msg.charAt(i);
+			if (msg.equals(rev))
+				msg2=" is a palindrome\n";
+			else
+				msg2=" is not a palindrome\n";
+			System.out.print(msg2);
+			InetAddress ip = InetAddress.getByName("127.0.0.1");
+			DatagramSocket ds2 = new DatagramSocket();
+			DatagramPacket dp2 = new DatagramPacket(msg2.getBytes(),msg2.length(),ip,4000);
+			ds2.send(dp2);
+			}
+
+		catch(Exception e){
+			System.out.println("Error");
+			}
+		}
+	}
